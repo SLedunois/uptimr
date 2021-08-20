a98fe22cbe5c41b196ed909a3bcbcf616a139e3b
Simon LEDUNOIS <simon.ledunois@gmail.com>
2021-08-20 09:48:06 +0200

    feat() Always redirect authenticated users to app page

--- src/main/java/fr/uptimr/AuthResource.java
+++ src/main/java/fr/uptimr/AuthResource.java
@@ -1,22 +1,38 @@
 package fr.uptimr;
 
 import io.quarkus.qute.Template;
-import io.quarkus.qute.TemplateInstance;
+import io.quarkus.security.identity.SecurityIdentity;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/auth")
 public class AuthResource {
+
+    @Inject
+    SecurityIdentity identity;
 
     @Inject
     Template signin;
 
     @GET
     @Path("/sign-in")
-    public TemplateInstance signin(@QueryParam("error") String error) {
-        return signin.data("error", error);
+    @Produces(MediaType.TEXT_HTML)
+    public Response signin(@QueryParam("error") String error) {
+        if (identity.isAnonymous()) {
+            Map<String, String> data = new HashMap<>();
+            data.put("error", error);
+            return Response.ok(signin.render(data)).build();
+        }
+
+        return Response.temporaryRedirect(URI.create("/")).build();
     }
 }
