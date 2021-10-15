247567500b0cf02086693ec3ce4bf966b26f1bcf
Simon LEDUNOIS <simon.ledunois@gmail.com>
2021-10-16 00:32:56 +0200

    fix() WelcomeMessage display UserIcon on midnight hour

--- src/main/ts/components/message.tsx
+++ src/main/ts/components/message.tsx
@@ -1,7 +1,7 @@
-import React, {FunctionComponent, ReactElement} from 'react'
-import {MoonIcon, SunIcon, SunriseIcon, SunsetIcon, UserIcon} from "../icons";
+import React, { FunctionComponent, ReactElement } from 'react'
+import { MoonIcon, SunIcon, SunriseIcon, SunsetIcon, UserIcon } from "../icons";
 import Badge from "./badge";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 type IWelcomeMessage = {
     name: string,
@@ -10,7 +10,7 @@ type IWelcomeMessage = {
 }
 
 function getKeyword(hour: number): string {
-    if ((hour > 21 && hour < 24) || (hour > 0 && hour <= 8)) {
+    if ((hour > 21 && hour < 24) || (hour >= 0 && hour <= 8)) {
         return 'welcome-message.night';
     } else if (hour > 8 && hour <= 13) {
         return 'welcome-message.morning'
@@ -24,17 +24,17 @@ function getKeyword(hour: number): string {
 }
 
 function getIcon(hour: number): ReactElement {
-    if ((hour > 21 && hour < 24) || (hour > 0 && hour <= 6)) {
-        return <MoonIcon/>
+    if ((hour > 21 && hour < 24) || (hour >= 0 && hour <= 6)) {
+        return <MoonIcon />
     } else if (hour > 6 && hour <= 9) {
-        return <SunriseIcon/>
+        return <SunriseIcon />
     } else if (hour > 9 && hour <= 18) {
-        return <SunIcon/>
+        return <SunIcon />
     } else if (hour > 18 && hour <= 21) {
-        return <SunsetIcon/>
+        return <SunsetIcon />
     }
 
-    return <UserIcon/>
+    return <UserIcon />
 }
 
 function getColor(hour: number): string {
@@ -49,14 +49,14 @@ function getColor(hour: number): string {
     return "pink"
 }
 
-export const WelcomeMessage: FunctionComponent<IWelcomeMessage> = ({name, className, hour}) => {
-    const {t} = useTranslation();
+export const WelcomeMessage: FunctionComponent<IWelcomeMessage> = ({ name, className, hour }) => {
+    const { t } = useTranslation();
     return (
         <h1 className={`flex flex-row items-center text-2xl font-normal ${className}`}>
             <Badge color={getColor(hour)} className="mr-4">
                 {getIcon(hour)}
             </Badge>
-            {t('welcome-message.message', {keyword: t(getKeyword(hour)), name})}
+            {t('welcome-message.message', { keyword: t(getKeyword(hour)), name })}
         </h1>
     )
 }
